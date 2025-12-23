b4bb0e4620275cea7434f42eb1a6e1a2b331a883
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 20:09:16 -0300

    Create exercicos BASICOS.py

---
+++ range/exercicos BASICOS.py
@@ -0,0 +1,23 @@
+#-- Imprima os números de 1 a 10 
+
+for i in range(1, 11): 
+    print(i) 
+    
+#------------------------------------------------------------------  
+
+#-- Imprima todos os números pares de 0 a 20 
+
+for i in range(0, 21, 2): 
+    print(i) 
+    
+#------------------------------------------------------------------------  
+
+#-- Some todos os números de 1 a 100 
+
+soma = 0 
+for i in range(1, 101): 
+    soma += i 
+print("Soma:", soma) 
+
+#-----------------------------------------------------------------
+  
