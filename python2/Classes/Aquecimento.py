3569fe5e631d7b596a16bc8b9a2ebf145cfeaa81
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 20:00:57 -0300

    Create Aquecimento.py

---
+++ python2/Classes/Aquecimento.py
@@ -0,0 +1,10 @@
+# Primeiro: função simples (sem classe ainda) 
+# Como guardar esses dados de várias pessoas?
+
+def apresentar_pessoa(nome, idade): 
+    print(f"Olá, meu nome é {nome} e tenho {idade} anos.") 
+ 
+apresentar_pessoa("Ana", 25) 
+apresentar_pessoa("Lucas", 30) 
+
+#Commits on May 4, 2025
