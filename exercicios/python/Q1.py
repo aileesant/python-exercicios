0fc8607dd4518df108438be3d427d2df5475db59
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 19:53:49 -0300

    Create Q1.py

---
+++ exercicios/python/Q1.py
@@ -0,0 +1,9 @@
+numeros = []
+
+for i in range(3):
+    valor = int(input('digite um número: '))
+    numeros.append(valor)
+
+maior = max(numeros)
+print(f'O maior número é: {maior}')    
+#Commits on Sep 28, 2024
