fca96ec0f86350af8200e9dff70c143f46f60d6e
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 19:49:37 -0300

    Create WhileSort.py

---
+++ exercicios/python/WhileSort.py
@@ -0,0 +1,20 @@
+#aula 3 do dia 28/09 2024
+
+numeros = []
+
+while True:
+    valor = int(input('Digite um valor: ')) #o int se põe quando é da variavel NUMERO
+    if(valor == 0):
+        break
+    else:
+        numeros.append(valor)
+
+print(numeros)        
+
+#foi criado um vator vazio
+#um laço infinito
+#quando digitar 0 acaba 
+#todos os numeros digitados antes do zero estao armazenados do "prédio" numeros
+#depois ele mostra os numeros digitados na tela dentro do 'numeros'
+
+--------------------------------------------------------------------------------------
