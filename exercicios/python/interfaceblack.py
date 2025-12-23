f69489de0b92677b9dab1ec063bf2e89bbf85432
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 19:48:58 -0300

    Create interfaceblack.py

---
+++ exercicios/python/interfaceblack.py
@@ -0,0 +1,56 @@
+import customtkinter as ctk 
+# o 'as' serve para apelidar o customtkinter com um nome menor
+from tkinter import messagebox 
+
+ctk.set_appearance_mode('dark') #o tema da biblioteca agr é emo
+
+#---------------- funções -------------------------------
+
+
+def calculo(): 
+    v = float(valor.get())
+    c = float(cotacao.get())
+    
+    valorfinal = v*c
+    messagebox.showinfo('Resultado', f'o valor em reais é :  R${valorfinal}')
+
+#--------------- by Aline --------------------------------
+
+janela = ctk.CTk()
+janela.geometry('400x250') #tamanho da janela
+janela.resizable(False,False) #para que a janela nao seja redimencionavel
+janela.title('Sistema de Conversão de Moedas')
+janela.iconbitmap('rocket_money_space_cohet_icon_210916.ico')
+
+
+ctk.CTkLabel(janela,
+             text= 'Sitema de Conversão',
+             text_color= 'hot pink',
+             font=('arial',25,'bold')).pack(pady=15) #criação do título
+
+valor = ctk.CTkEntry(janela,
+                     width=350,
+                     height=45,
+                     placeholder_text='Digite o valor em dólar: ') #pra add um textinho lá
+valor.pack()
+
+cotacao = ctk.CTkEntry(janela,
+                     width=350,
+                     height=45,
+                     placeholder_text='Digite a cotação atual do dólar: ') 
+cotacao.pack(pady=10) #o 'pady' serve para dar espaçamento entre as janelas e tal
+
+
+botao = ctk.CTkButton(janela,
+                      text='Calcular',
+                      width=150,
+                      height=40,
+                      fg_color='hot pink',
+                      text_color='black',
+                      command=calculo) #funcao para fazer o calculo da conversao apos clicar no botao
+botao.pack(pady=10) 
+
+
+janela.mainloop() #mainloop é o 'start' 
+
+#Commits on Oct 5, 2024
