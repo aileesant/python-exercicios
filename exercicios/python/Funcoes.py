a2ab1637f0897becd1d8d4fd73a9efc205257c70
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 19:54:20 -0300

    Create Funcoes.py

---
+++ exercicios/python/Funcoes.py
@@ -0,0 +1,12 @@
+def  soma(valor1, valor2):     #a função tem que tá no inicio do código
+    total = valor1 + valor2
+    print(f'A soma dos dois valores é: {total}')
+
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+#pra copiar uma linha pra linha debaixo ctrl + sift + seta pra baixo
+
+soma(valor1, valor2) #chamada da fução 
+
+#Commits on Sep 28, 2024
